d5bc5a4399cb018d54389d99b0bc90144c5f868c
Luke Confait <luke.confait@gmail.com>
2023-02-20 16:16:57 +0200

    More refactoring

--- Training.py
+++ Training.py
@@ -1,11 +1,12 @@
-import matplotlib.pyplot as plot
+import os
+import matplotlib.pyplot as plt
 import numpy as np
 
 from scipy.io import loadmat
 
 
 class Linear:
-    """A single linear layer with a logistic activation function,  with weights and biases"""
+    """A single linear layer model with a logistic activation function,  with weights and biases"""
 
     def __init__(
         self,
@@ -69,7 +70,7 @@ class Linear:
             if i % 100 == 0:
                 print(f"At epoch:{i}, the loss was {cost}")
 
-    def load_model(self, folder_path: str):
+    def load(self, folder_path: str):
         """Load in the weights and biases from the saved model"""
         self.weights_1 = np.loadtxt(f"{folder_path}\w1.csv", delimiter=",")
         self.biases_1 = np.loadtxt(f"{folder_path}\\b1.csv", delimiter=",")
@@ -92,6 +93,18 @@ class Linear:
         """Cost function for the error of the model"""
         return -(labels * np.log(outputs))
 
+    def save(self):
+        path = f"weights and biases/M={self.L}"
+        try:
+            os.mkdir(path)
+        except OSError as error:
+            print(error)
+
+        np.savetxt(f"{path}/w1.csv", self.weights_1, delimiter=",")
+        np.savetxt(f"{path}/b1.csv", self.biases_1, delimiter=",")
+        np.savetxt(f"{path}/w2.csv", self.weights_2, delimiter=",")
+        np.savetxt(f"{path}/b2.csv", self.biases_2, delimiter=",")
+
 
 def y2indicator(training_labels):
     """Converts output to 1X10 matrix where the 1 indicates the number from 0 to 9"""
@@ -104,12 +117,13 @@ def y2indicator(training_labels):
 
 
 def data_split(data, labels, split):
-    # split from 0 to 1
+    """split the data and labels by a fraction betwee 0 to 1"""
     data_length = split * len(data)
     training_data = data.tolist()
     training_labels = labels.tolist()
     test_data = []
     test_labels = []
+
     while len(training_data) > data_length:
         random = np.random.randint(0, len(training_data))
         test_data.append(training_data.pop(random))
@@ -123,18 +137,14 @@ def data_split(data, labels, split):
     )
 
 
-def gen_confusion_matrix(model, test_data, test_labels):
+def plot_confusion_matrix(model, test_data, test_labels):
     confusion_matrix = np.zeros((10, 10))
     for i, data in enumerate(test_data):
         result = np.argmax(model.model_test(data))
         confusion_matrix[int(test_labels[i])][result] += 1
-    return confusion_matrix
-
-
-def plot_confusion_matrix(confusion_matrix):
     # Set up figure and axes
-    fig, ax = plot.subplots(figsize=(7.5, 7.5))
-    ax.matshow(confusion_matrix, cmap=plot.cm.Blues, alpha=0.7)
+    fig, ax = plt.subplots(figsize=(7.5, 7.5))
+    ax.matshow(confusion_matrix, cmap=plt.cm.Blues, alpha=0.7)
 
     for i in range(confusion_matrix.shape[0]):
         for j in range(confusion_matrix.shape[1]):
@@ -146,7 +156,7 @@ def plot_confusion_matrix(confusion_matrix):
                 ha="center",
                 size="xx-large",
             )
-    plot.show()
+    return ax
 
 
 def nest_list(list1, rows, columns):
@@ -161,6 +171,18 @@ def nest_list(list1, rows, columns):
     return result
 
 
+def test(test_data, test_labels, model, j):
+
+    model_prediction = np.argmax(model.model_test(test_data[j]))
+    label = test_labels[j]
+
+    fig, ax = plt.subplots()
+    plt.imshow(nest_list(test_data[j], 28, 28))
+    print(f"test label: {label}, model prediction: {model_prediction}")
+
+    # return ax
+
+
 def main():
     # Data import
     mnist = loadmat(r"input\mnist-original.mat")
@@ -170,10 +192,10 @@ def main():
 
     # Hyperparameters
     input_length = 784
-    hidden_layer_length = 100
+    hidden_layer_length = 2
     output_length = 10
     train_rate = 1e-5
-    epochs = 1000
+    epochs = 10
 
     # Split into the training and test set
     training_data, training_labels, test_data, test_labels = data_split(
@@ -186,52 +208,28 @@ def main():
     # Create the model of one Linear layer mappping input to output
     model = Linear(hidden_layer_length, input_length, output_length)
 
-    train_model = True
+    train_model = False
     save_model = True
     # Train the model
     if train_model:
-        model.train(
-            epochs,
-            training_data,
-            training_labels,
-            train_rate,
-        )
+        model.train(epochs, training_data, training_labels, train_rate)
         if save_model:
-            np.savetxt(
-                f"weights and biases/M={hidden_layer_length}/w1.csv",
-                model.weights_1,
-                delimiter=",",
-            )
-            np.savetxt(
-                f"weights and biases/M={hidden_layer_length}/b1.csv",
-                model.biases_1,
-                delimiter=",",
-            )
-            np.savetxt(
-                f"weights and biases/M={hidden_layer_length}/w2.csv",
-                model.weights_2,
-                delimiter=",",
-            )
-            np.savetxt(
-                f"weights and biases/M={hidden_layer_length}/b2.csv",
-                model.biases_2,
-                delimiter=",",
-            )
+            model.save()
     else:
-        model.load_model("weights and biases/M=50")
+        model.load(f"weights and biases/M={hidden_layer_length}")
 
     # k = rand.randint(0, len(test_data))
     # np.savetxt("weights and biases\example.csv", test_data[k], delimiter=",")
 
     # generate confusion matrix for the models predictions of the test set
-    confusion_matrix = gen_confusion_matrix(model, test_data, test_labels)
-    plot_confusion_matrix(confusion_matrix)
+    plot_confusion_matrix(model, test_data, test_labels)
+    plt.show()
 
-    j = np.random.randint(1, 2000)
-    print(model.model_test(test_data[j]))
-    plot.imshow(nest_list(test_data[j], 28, 28))
-    print(test_labels[j])
-    plot.show()
+    # confusion_matrix.savefig("../")
+
+    j = np.random.randint(1, len(test_data))
+    test(test_data, test_labels, model, j)
+    plt.show()
 
 
 if __name__ == "__main__":
